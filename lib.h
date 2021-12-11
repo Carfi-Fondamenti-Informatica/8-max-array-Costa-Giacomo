cbfbbf84a0f76fc6d939b578fac47fe2d2ae56ea
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-11 12:42:05 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef UNTITLED15_LIB_H
 #define UNTITLED_15_LIB_H
-float numeroMassimo(const float numeri[], int dimensione);
+float numeromassimo(const float numeri[], int dimensione);
 #endif //UNTITLED_15_H
