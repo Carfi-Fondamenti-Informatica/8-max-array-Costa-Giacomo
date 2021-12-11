9a8f457481c33082c76736adcb21a98a7217e6f3
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-11 12:25:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "lib.h"
 using namespace std;
 int main() {
-    int dimensione;
-    cin >> dimensione;
-    float numeri [dimensione];
-    for (int i=0; i<dimensione; i++){
+    int dim;
+    cin >> dim;
+    float numeri [dim];
+    for (int i=0; i<dim; i++){
         cin >> numeri[i];
     }
-    maxnum(numeri, dimensione);
-    cout <<  maxnum(numeri, dimensione) << endl;
+    numeromassimo(numeri, dim);
+    cout <<  numeromassimo(numeri, dimensione) << endl;
     return 0;
 }
