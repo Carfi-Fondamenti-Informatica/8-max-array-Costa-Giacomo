7a069676b126f7cd60608e4a56500da71f36f293
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-11 12:19:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+float maxnum (const float numeri [], int dim){
+
+    numeri[dim];
+    float max=numeri [0];
+    for(int i=0; i<dim; i++){
+        if(max < numeri [i]){
+            max=numeri[i];
+        }
+    }
+    retun max;
+}
